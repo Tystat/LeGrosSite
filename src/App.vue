ae70c50566000e1e51a7e33c563d0067f55d2a09
Thomas Tabuteau <thomas-tabuteau@orange.fr>
2021-01-10 17:16:38 +0100

    Setup the API calls
    Added a proxy to allow third party API calls and added a simple call to the Riot API
    Also renamed the main page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <img alt="LGC logo" width="200" src="./assets/logo_lgc_final.png">
-    <HelloWorld msg="Bienvenu sur le gros site !"/>
+    <MainPage msg="Bienvenu sur le gros site !"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/MainPage.vue'
+import MainPage from './components/MainPage.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainPage
   }
 }
 </script>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -7,6 +7,7 @@
       <b-form inline @submit="onSubmit" class="justify-content-center">
             <b-form-input
                   id="summoner_name"
+                  v-model="summoner_name"
                   placeholder="Summoner's Name"
                   required
                   class="mb-2 mr-sm-2 mb-sm-0"
@@ -15,6 +16,7 @@
             <b-input-group prepend="#">
               <b-form-select
                     id="summoner_region"
+                    v-model="summoner_region"
                     placeholder="Region"
                     :options="regions"
                     required
@@ -30,20 +32,33 @@
 </template>
 
 <script>
+
 export default {
   name: 'MainPage',
   props: {
     msg: String
   },
   data() {
-    return {
-      regions: ['BR','EUNE','EUW','LAN','LAS','NA','OCE','RU','TR','JP','KR','PBE']
+    return{
+      regions: ['BR','EUNE','EUW','LAN','LAS','NA','OCE','RU','TR','JP','KR'],
+      summoner_name: null,
+      summoner_region: null,
     }
   },
   methods: {
     onSubmit(event) {
       event.preventDefault()
+      fetch(`/api/${this.summoner_region}/lol/summoner/v4/summoners/by-name/${this.summoner_name}`)
+      .then(function(response) {
+        // The response is a Response instance.
+        // You parse the data into a useable format using `.json()`
+        return response.json();
+        }).then(function(data) {
+        // `data` is the parsed version of the JSON returned from the above endpoint.
+        console.log(data);
+        });
     }
-  }
+  },
+
 }
 </script>
