af42948c8e876a38b155264a06342111eeb19823
AdrienChotard <adrien.chotard@hotmail.com>
2021-02-02 18:06:42 +0100

    Summoner Masteries : API call to get champions names and champions values are now formatted

--- src/components/summonerMasteries.vue
+++ src/components/summonerMasteries.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="summonerMasteries">
-        <b-card>
+        <b-card 
+        class="mb-2 text-dark mx-auto mt-5">
             <b-card-header class="text-reset">Maitrises</b-card-header>
             <b-card-body>
                 <b-button-group>
-                    <b-button v-on:click="sort = Sort.Level">Niveau</b-button>
-                    <b-button v-on:click="sort = Sort.Points">Points</b-button>
-                    <b-button v-on:click="sort = Sort.Alphabetical">Alphabétique</b-button>
+                    <b-button @click="() => sort = Sort.Level">Niveau</b-button>
+                    <b-button @click="() => sort = Sort.Points">Points</b-button>
+                    <b-button @click="() => sort = Sort.Alphabetical">Alphabétique</b-button>
                 </b-button-group>
-                <li v-for="champ in champions.slice(0,5)" :key="champ.name">
+                <li v-for="champ in this.formattedChampions.slice(0,10)" :key="champ.name">
+                    <img :src="'https://ddragon.leagueoflegends.com/cdn/11.2.1/img/champion/'+champ.name+'.png'" width="5%"/>
                     {{champ.name}} || Mastery {{champ.mastery}} : {{champ.points}} pts
                 </li>
             </b-card-body>
@@ -22,13 +24,15 @@ const Sort = {
     Level: 0,
     Points: 1,
     Alphabetical: 2
-}
+};
 
 export default {
     name: 'summonerMasteries',
     data: function() {
         return{
-            sort: Sort.Level
+            sort: Sort.Level,
+            formattedChampions: [],
+            Sort
         }
     },
     computed: {
@@ -39,32 +43,52 @@ export default {
     },
     methods: {
         sortChamps(){
+            fetch('/api/ddragonChampions').then((champs) => {
+                //Parse the API response
+                return champs.json();
+            }).then((dataChampions) => {
+                this.formattedChampions = []
+                for(var i = 0; i < this.champions.length; i++){
+                    for(var dChampion in dataChampions.data){
+                        if(dataChampions.data[dChampion].key == this.champions[i].championId){
+                            this.formattedChampions.push({name: dChampion, mastery: this.champions[i].championLevel, points: this.champions[i].championPoints});
+                            break;
+                        }
+                    }
+                }
+              });
+
             switch(this.sort){
                 case Sort.Level:
-                    this.champions.sort((a, b) => 
-                        (a.championLevel > b.championLevel) ?
+                    this.formattedChampions.sort((a, b) => 
+                        (a.mastery < b.mastery) ?
                             1 :
-                            (a.championLevel === b.championLevel) ?
-                                ((a.championPoints > b.championPoints) ?
+                            (a.mastery === b.mastery) ?
+                                ((a.points < b.points) ?
                                     1 :
                                     -1) :
                                 -1);
                     break;
                 case Sort.Points:
-                    this.champions.sort((a, b) => (a.championPoints > b.championPoints) ? 1 : -1);
+                    this.formattedChampions.sort((a, b) => (a.points < b.points) ? 1 : -1);
                     break;
                 case Sort.Alphabetical:
-                    this.champions.sort((a, b) => ((a.toLowerCase().replace(/\s/g, '') > (b.toLowerCase().replace(/\s/g, ''))) ? 1 : -1));
+                    this.formattedChampions.sort((a, b) => ((a.name.toLowerCase().replace(/\s/g, '') > (b.name.toLowerCase().replace(/\s/g, ''))) ? 1 : -1));
+                    break;
             }
+            console.log(this.formattedChampions)
         }
     },
+    mounted(){
+        this.sortChamps();
+    },
     watch: {
-        sortType(){
+        sort: function(){
             this.sortChamps();
         }
     },
     props: {
-        champions: []
+        champions: Array
     }
 }
 </script>
