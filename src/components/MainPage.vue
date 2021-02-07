7474d15a9cae724294fe470524b276b0f4818905
Thomas Tabuteau <thomas-tabuteau@orange.fr>
2021-02-07 15:48:45 +0100

    Changed logo

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="MainPage">
-    <img alt="LGC logo" width="200" src="../assets/logo_2.png">
+    <img alt="LGC logo" width="200" src="../assets/logo_3.png">
     <h1>Bienvenue sur League Helper !</h1>
     <p>
       Ce site va vous permettre de consulter vos statistiques sur League of Legends et de les comparer avec celles de vos amis !
